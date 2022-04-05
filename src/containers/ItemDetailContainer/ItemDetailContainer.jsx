5cd6e84ed7b46400ea796bea9580bbfff9c2081f
lucianoblanco <lucianoblanco1994@gmail.com>
2022-04-05 19:56:03 -0300

    Agregamos rutas de imagenes, luego ver como poner imagenes locales

--- src/containers/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/containers/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,24 +2,8 @@ import React, { useEffect, useState} from 'react';
 import { ItemDetail } from './ItemDetail';
 
 const ItemDetailContainer = () => {
-    // const item = [{"id":34234,"name":"Moto E20","price":22999,"storage":"32 GB","ram":"2 GB","stock":25,"url":"https://http2.mlstatic.com/D_NQ_NP_2X_832761-MLA48048712904_102021-F.webp"}];
     const [product, setProduct] = useState([]);
     const [loading, setLoading] = useState(true);
-    // const promesa = new Promise((res, rej) => {
-    //     setTimeout(() => {
-    //         res(item);            
-    //         }, 1500)
-    // });
-    // useEffect(() => {
-
-    //     promesa.then((product) =>{
-    //         setProduct(product);
-    //     }).catch((error) => {
-    //         console.error('Error: ', error);
-    //     }).finally(() => {
-    //         setLoading(false);
-    //     })
-    // }, [])
 
     useEffect(() => {
         const url = "https://mocki.io/v1/7fcc394d-27bb-4ee7-b193-1bce5067eeda";
@@ -40,22 +24,68 @@ const ItemDetailContainer = () => {
         getitem()
     }, []);
 
+    const item = product.map((element) => {
+        return (
+            <div style={itemStyle} key={element.id}>
+                <h4 style={itemTitle}>{element.name}</h4>
+                <img style={imagen} src={element.url} alt={element.name} />
+                <div style={label}>
+                    <p style={details}>Price:</p>
+                    <span style={details}>$ {element.price}</span>
+                </div>
+                <div style={label}>
+                    <p style={details}>Stock:</p>
+                    <span style={details}>{element.stock}</span>
+                </div>
+                <div style={label}>
+                    <p style={details}>ID:</p>
+                    <p style={details}>{element.id}</p>
+                </div>
+                <div style={label}>
+                    <p style={details}>Almacenamiento:</p>
+                    <span style={details}>{element.storage}</span>
+                </div>
+                <div style={label}>
+                    <p style={details}>RAM:</p>
+                    <span style={details}>{element.ram}</span>
+                </div>
+            </div>
+        )
+    }) 
+
     return (
         loading
-            ? (
-                <p>Detalles</p>
-            )
-            : (
-                <div style={containerDetail}>
-                    <ItemDetail product={product} />
-                </div>)
+            ? (<p>Detalles</p>)
+            : (<ItemDetail product={item} />)
     )
 }
 
 export default ItemDetailContainer
 
-const containerDetail = {
-    margin: 30,
+const label = {
     display: 'flex',
-    justifyContent: 'center',
+    justifiContent: 'start',
+    flexFlow: 'nowrap row',
+    padding: 10,
+    margin: 2,
+}
+
+const details = {
+    fontSize: 18,
+    color: '#111111',
+    margin: '0 5px 0 5px',
+    fontWeight: 500,
+}
+
+const imagen = {
+    width: '100%',
+}
+
+const itemStyle = {
+    width: 250,
+}
+const itemTitle = {
+    fontSize: 24,
+    fontWeight: 600,
+    textAlign: 'center',
 }
